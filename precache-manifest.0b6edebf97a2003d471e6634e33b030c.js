97adcd98f9dbeea2372829932648caa56b5997bf
isanjaymaurya <its.sanjaymaurya@gmail.com>
2022-09-13 15:53:48 +0530

    Updates

---
+++ precache-manifest.0b6edebf97a2003d471e6634e33b030c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfb7d26fd1980b239470",
+    "url": "/react-weather-app/static/css/main.3419737a.chunk.css"
+  },
+  {
+    "revision": "dfb7d26fd1980b239470",
+    "url": "/react-weather-app/static/js/main.848e758f.chunk.js"
+  },
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  },
+  {
+    "revision": "0cfe4d860abfb23c2750",
+    "url": "/react-weather-app/static/js/2.a20dbca5.chunk.js"
+  },
+  {
+    "revision": "44adf090a255505c5a039c121e77672b",
+    "url": "/react-weather-app/index.html"
+  }
+];
