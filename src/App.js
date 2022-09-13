5f52bc0a76eb0b0649c841fb63737a368166d6f2
isanjaymaurya <its.sanjaymaurya@gmail.com>
2022-09-13 15:20:02 +0530

    Deploy React app to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,60 +1,53 @@
-import React, { useState } from 'react';
-
-import './App.css';
-
-import { fetchWeather } from './api/fetchWeather';
-
-// class App extends Component {
-//     render(){
-//         return (
-//             <div>het</div>
-//         )
-//     }
-// }
+import React, { useState } from "react";
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
 
 const App = () => {
-    
-    const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState({});
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
-    const search = async (e) => {
-        if(e.key === 'Enter'){
-            const data = await fetchWeather(query);
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
 
-            setWeather(data);
-            setQuery('');
-            console.log(data)
-        }
+      setWeather(data);
+      setQuery("");
+      console.log(data);
     }
+  };
 
-    return (
-        <div className="main-container">
-            <input
-                type="text"
-                className="search"
-                placeholder="search .."
-                value={query}
-                onChange={(event) => setQuery(event.target.value)}
-                onKeyPress={search}
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="search .."
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
             />
-            {weather.main && (
-                <div className="city">
-                    <h2 className="city-name">
-                        <span>{weather.name}</span>
-                        <sup>{weather.sys.country}</sup>
-                    </h2>
-                    <div className="city-temp">
-                        {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
-                    </div>
-                    <div className="info">
-                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                        <p>{weather.weather[0].description}</p>
-                    </div>
-                </div>
-            )}
+            <p>{weather.weather[0].description}</p>
+          </div>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
